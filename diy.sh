e962adbeb73b860bea68cd7522210ad3312e16c3
owlboy-lin <60811203+owlboy-lin@users.noreply.github.com>
2020-04-19 22:04:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,12 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Import external feeds
+git clone https://github.com/superstarfly/openwrt-package package/lienol
+git clone https://github.com/vernesong/OpenClash package/openclash
+git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR package/koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardhome
+git clone https://github.com/frainzy1477/luci-app-clash package/clash
+git clone https://github.com/fw876/helloworld package/ssrplus
+# git clone 其他github插件源码地址 package/文件夹名称
