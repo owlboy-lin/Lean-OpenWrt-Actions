bfa5aaabcd40137b6df0d7e085b4de74ed2205c7
owlboy-lin <60811203+owlboy-lin@users.noreply.github.com>
2020-06-03 11:24:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -24,11 +24,12 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardh
 
 git clone https://github.com/vernesong/OpenClash package/openclash
 
-git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR package/koolproxyR
-
 git clone https://github.com/frainzy1477/luci-app-clash package/clash
 
 git clone https://github.com/fw876/helloworld package/ssrplus
+
+git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR package/koolproxyR
+
 # git clone 其他github插件源码地址 package/文件夹名称
 
 
