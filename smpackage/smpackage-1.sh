6e43428d2766cadd66c93e771724e8355eaacdcc
GitHub Action <github-actions[bot]@github.com>
2024-04-26 15:59:19 +0000

    Sync files 1196b8dc7528f298343ce631df422d80bc373383

--- smpackage/smpackage-1.sh
+++ smpackage/smpackage-1.sh
@@ -100,6 +100,7 @@
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
 sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
