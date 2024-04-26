1196b8dc7528f298343ce631df422d80bc373383
owlboy-lin <298076473@qq.com>
2024-04-26 20:23:55 +0800

    update

--- smpackage/smpackage-2-all.sh
+++ smpackage/smpackage-2-all.sh
@@ -67,32 +67,32 @@
 
 
 
-# openclash
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
-# 加入OpenClash核心
-# chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
-# if [ "$1" = "rk33xx" ]; then
-#     $GITHUB_WORKSPACE/preset-clash-core.sh arm64
-# elif [ "$1" = "rk35xx" ]; then
-#     $GITHUB_WORKSPACE/preset-clash-core.sh arm64
-# elif [ "$1" = "x86" ]; then
-#     $GITHUB_WORKSPACE/preset-clash-core.sh amd64
-# fi
+# # openclash
+# svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
+# # svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
+# # 加入OpenClash核心
+# # chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+# # if [ "$1" = "rk33xx" ]; then
+# #     $GITHUB_WORKSPACE/preset-clash-core.sh arm64
+# # elif [ "$1" = "rk35xx" ]; then
+# #     $GITHUB_WORKSPACE/preset-clash-core.sh arm64
+# # elif [ "$1" = "x86" ]; then
+# #     $GITHUB_WORKSPACE/preset-clash-core.sh amd64
+# # fi
 
-# adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+# # adguardhome
+# svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+# svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
+# # svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+# # svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 
-# mosdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/mosdns package/mosdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/v2dat package/v2dat
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/mosdns package/mosdns
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/v2dat package/v2dat
+# # mosdns
+# svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
+# svn export https://github.com/kenzok8/openwrt-packages/trunk/mosdns package/mosdns
+# svn export https://github.com/kenzok8/openwrt-packages/trunk/v2dat package/v2dat
+# # svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
+# # svn export https://github.com/kiddin9/openwrt-packages/trunk/mosdns package/mosdns
+# # svn export https://github.com/kiddin9/openwrt-packages/trunk/v2dat package/v2dat
 
 
 # 添加自定义软件包
