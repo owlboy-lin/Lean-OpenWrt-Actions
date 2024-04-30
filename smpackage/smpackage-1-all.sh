29df83cceea2de35053ce825dd935cc020ecd93a
owlboy-lin <298076473@qq.com>
2024-04-30 17:05:51 +0800

    update

--- smpackage/smpackage-1-all.sh
+++ smpackage/smpackage-1-all.sh
@@ -99,10 +99,9 @@
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+## small-package-all
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-# sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
