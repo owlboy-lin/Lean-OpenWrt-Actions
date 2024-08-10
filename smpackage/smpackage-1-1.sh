de3873da09f004a2d7c0f619eb4f603dcc734112
owlboy-lin <298076473@qq.com>
2024-08-10 18:38:42 +0800

    Update smpackage-1-1.sh

--- smpackage/smpackage-1-1.sh
+++ smpackage/smpackage-1-1.sh
@@ -103,5 +103,5 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
