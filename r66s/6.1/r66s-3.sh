d35f10cd531c8b1cb7c9f4308472f88e8d4de175
owlboy-lin <298076473@qq.com>
2024-10-09 21:41:28 +0800

    update

--- r66s/6.1/r66s-3.sh
+++ r66s/6.1/r66s-3.sh
@@ -142,6 +142,12 @@ CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y
 CONFIG_PACKAGE_luci-app-passwall2=y
 CONFIG_PACKAGE_luci-i18n-passwall2-zh-cn=y
 
+# VPN
+CONFIG_PACKAGE_luci-app-ipsec-vpnd=y
+CONFIG_PACKAGE_luci-i18n-ipsec-vpnd-zh-cn=y
+
+CONFIG_PACKAGE_luci-app-openvpn=y
+CONFIG_PACKAGE_luci-i18n-openvpn-zh-cn=y
 
 # quickstart
 CONFIG_PACKAGE_luci-app-quickstart=y
