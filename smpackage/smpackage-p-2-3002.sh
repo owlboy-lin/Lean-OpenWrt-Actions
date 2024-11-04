f9264a6c1c4411f12a07e2b61ae610557d6b66fa
owlboy-lin <298076473@qq.com>
2024-11-04 11:54:22 +0800

    Update smpackage-p-2-3002.sh

--- smpackage/smpackage-p-2-3002.sh
+++ smpackage/smpackage-p-2-3002.sh
@@ -16,7 +16,7 @@ sed -i 's/192.168.1.1/192.168.89.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
-# sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/BLLW3002/g' package/base-files/files/bin/config_generate
 
 # 替换终端为bash
 # sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
