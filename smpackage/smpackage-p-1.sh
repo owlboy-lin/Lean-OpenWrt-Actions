e62a7cec0bce2080449f55ae6112531feb2a89b8
GitHub Action <github-actions[bot]@github.com>
2024-11-06 15:59:02 +0000

    Sync files bd7b8b57f202d94eb6830ebf29c6862ba7d0c620

--- smpackage/smpackage-p-1.sh
+++ smpackage/smpackage-p-1.sh
@@ -11,7 +11,7 @@
 # =================================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.24.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.24.1/g' package/base-files/files/bin/config_generate
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
