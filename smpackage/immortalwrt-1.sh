4a80150e4e802ea9361201726da6daab9d97a6fe
owlboy-lin <298076473@qq.com>
2025-02-03 13:30:11 +0800

    update

--- smpackage/immortalwrt-1.sh
+++ smpackage/immortalwrt-1.sh
@@ -14,7 +14,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
@@ -66,21 +66,23 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 
 #  下载源码
 ## adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+# git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 
 ## autoreboot
 # git clone https://github.com/f8q8/luci-app-autoreboot package/luci-app-autoreboot
 
 ## mosdns
-echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+# echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 
 ## netspeedtest
 # echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
 # 
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+# git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 
 ## OpenClash
-echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+# echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
@@ -94,7 +96,7 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.def
 ## poweroff
 # echo 'src-git poweroff https://github.com/esirplayground/luci-app-poweroff' >>feeds.conf.default
 
-git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+# git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
@@ -107,4 +109,65 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # iStore
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+# echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
+
+
+#!/bin/bash
+
+#删除feeds中的插件
+# rm -rf ./feeds/packages/net/mosdns
+# rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/net/geoview
+rm -rf ./feeds/packages/net/shadowsocks-libev
+rm -rf ./feeds/packages/net/chinadns-ng
+
+
+#克隆依赖插件
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+
+
+#克隆的源码放在small文件夹
+mkdir package/small
+pushd package/small
+
+
+#adguardhome
+git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+
+#lucky
+# git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
+
+# #smartdns
+# git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
+# git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
+
+# #ssrp
+# git clone -b master --depth 1 https://github.com/fw876/helloworld.git
+
+# #passwall
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+
+#passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+
+# #mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+
+# #openclash
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+
+## helloworld
+git clone https://github.com/fw876/helloworld.git
+
+## poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff.git
+
+## iStore
+git clone https://github.com/linkease/istore.git
+
+
+
+popd
+
+echo "packages executed successfully!"
