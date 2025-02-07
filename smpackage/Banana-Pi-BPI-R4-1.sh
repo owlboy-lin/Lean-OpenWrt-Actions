c5a47fd14ccc1ae4a3f8922564fd81e7794fe06c
owlboy-lin <298076473@qq.com>
2025-02-07 14:37:20 +0800

    Update Banana-Pi-BPI-R4-1.sh

--- smpackage/Banana-Pi-BPI-R4-1.sh
+++ smpackage/Banana-Pi-BPI-R4-1.sh
@@ -143,7 +143,7 @@ git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
 
 # #ssrp
-git clone -b master --depth 1 https://github.com/fw876/helloworld.git
+# git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 
 # #passwall
 # git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
@@ -155,7 +155,7 @@ git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 
 # #openclash
-git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 
 ## poweroff
 git clone -b master https://github.com/esirplayground/luci-app-poweroff.git
