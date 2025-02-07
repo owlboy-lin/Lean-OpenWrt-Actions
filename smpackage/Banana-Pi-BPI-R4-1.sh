ded8ed93cd27b1b7860db6fbc00dbc6418ecc8c6
owlboy-lin <298076473@qq.com>
2025-02-08 00:18:49 +0800

    Update Banana-Pi-BPI-R4-1.sh

--- smpackage/Banana-Pi-BPI-R4-1.sh
+++ smpackage/Banana-Pi-BPI-R4-1.sh
@@ -118,13 +118,13 @@
 #删除feeds中的插件
 # rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/v2ray-geodata
-rm -rf ./feeds/packages/net/geoview
-rm -rf ./feeds/packages/net/shadowsocks-libev
-rm -rf ./feeds/packages/net/chinadns-ng
+# rm -rf ./feeds/packages/net/geoview
+# rm -rf ./feeds/packages/net/shadowsocks-libev
+# rm -rf ./feeds/packages/net/chinadns-ng
 
 
 #克隆依赖插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
 
 
 #克隆的源码放在small文件夹
