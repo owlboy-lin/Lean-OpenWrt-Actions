00a3f16b3e923bfa711a9fcf8a158e57a3595062
owlboy-lin <298076473@qq.com>
2025-02-23 23:52:37 +0800

    update

--- smpackage/openwrt-packages-small-1.sh
+++ smpackage/openwrt-packages-small-1.sh
@@ -100,6 +100,7 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+cd openwrt
 ./scripts/feeds update -a 
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
--- smpackage/small-package-1.sh
+++ smpackage/small-package-1.sh
@@ -96,6 +96,7 @@
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+cd openwrt
 ./scripts/feeds update -a
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
