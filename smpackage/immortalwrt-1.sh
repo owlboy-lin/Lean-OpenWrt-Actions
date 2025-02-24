537c690b7b9eb95ddd7054abf681278c8c0cf57e
owlboy-lin <298076473@qq.com>
2025-02-24 22:34:03 +0800

    update

--- smpackage/immortalwrt-1.sh
+++ smpackage/immortalwrt-1.sh
@@ -10,6 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # =================================================================
 
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.24.1/g' package/base-files/files/bin/config_generate
+
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
@@ -23,9 +26,6 @@
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
 # git clone https://github.com/kenzok8/small.git package/small
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Import external feeds
 # git clone https://github.com/Lienol/openwrt-package.git package/lienol
@@ -68,8 +68,6 @@
 ## adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
-# git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
-
 ## autoreboot
 # git clone https://github.com/f8q8/luci-app-autoreboot package/luci-app-autoreboot
 
@@ -77,8 +75,6 @@
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 
 ## netspeedtest
-# echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
-# 
 # git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 
 ## OpenClash
@@ -94,80 +90,31 @@
 
 
 ## poweroff
-# echo 'src-git poweroff https://github.com/esirplayground/luci-app-poweroff' >>feeds.conf.default
-
 # git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-## small-package-all
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+# sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# cd openwrt
+# ./scripts/feeds update -a
+# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# ./scripts/feeds update -a 
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+# rm -rf feeds/packages/utils/v2dat
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+
+
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 
 # iStore
 # echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-
-
-
-#!/bin/bash
-
-#删除feeds中的插件
-# rm -rf ./feeds/packages/net/mosdns
-# rm -rf ./feeds/packages/net/v2ray-geodata
-rm -rf ./feeds/packages/net/geoview
-rm -rf ./feeds/packages/net/shadowsocks-libev
-rm -rf ./feeds/packages/net/chinadns-ng
-
-
-#克隆依赖插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
-
-
-#克隆的源码放在small文件夹
-mkdir package/small
-pushd package/small
-
-
-#adguardhome
-git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
-
-#lucky
-# git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
-
-# #smartdns
-# git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
-# git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
-
-# #ssrp
-git clone -b master --depth 1 https://github.com/fw876/helloworld.git
-
-# #passwall
-# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
-
-#passwall2
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
-
-# #mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
-
-# #openclash
-git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
-
-## poweroff
-git clone -b master https://github.com/esirplayground/luci-app-poweroff.git
-
-## iStore
-git clone -b main https://github.com/linkease/istore.git
-
-## netspeedtest
-git clone -b master https://github.com/sirpdboy/netspeedtest.git
-
-
-
-popd
-
-echo "packages executed successfully!"
