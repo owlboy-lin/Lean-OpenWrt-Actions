b49083e7a6bafb0b51a6ae78f5faef8a94528068
owlboy-lin <298076473@qq.com>
2025-02-27 13:22:12 +0800

    Update immortalwrt-all-1.sh

--- smpackage/immortalwrt-all-1.sh
+++ smpackage/immortalwrt-all-1.sh
@@ -90,8 +90,11 @@ echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.con
 
 
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+
+
+
 ## passwall
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 ## passwall2
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
