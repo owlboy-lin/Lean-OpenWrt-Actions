640255ce1ec335c4a98b78166dbe77d14b1c6109
owlboy-lin <298076473@qq.com>
2025-02-28 18:50:20 +0800

    update

--- smpackage/immortalwrt-small-package-1.sh
+++ smpackage/immortalwrt-small-package-1.sh
@@ -102,7 +102,7 @@
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 ## small-package-all
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
