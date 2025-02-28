f26c32ba882afa59c83a87c9f2ed5915c447fea1
owlboy-lin <298076473@qq.com>
2025-02-28 20:28:13 +0800

    Update bllw3002-immortalwrt-1.sh

--- smpackage/bllw3002-immortalwrt-1.sh
+++ smpackage/bllw3002-immortalwrt-1.sh
@@ -104,12 +104,35 @@
 ## small-package-all
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
+# sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# cd openwrt
+# ./scripts/feeds update -a
+# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
+## openwrt-packages-small
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# cd openwrt
+# ./scripts/feeds update -a 
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+# rm -rf feeds/packages/utils/v2dat
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# iStore
-# echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
+
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+
+# # iStore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
+
+cd openwrt
+./scripts/feeds update -a
+rm -rf feeds/luci/applications/luci-app-passwall
 
 
 
