3c8dcae227f73b344115fc9ec4d6043993c995ed
owlboy-lin <298076473@qq.com>
2025-03-05 13:11:37 +0800

    Update immortalwrt-all-2.sh

--- smpackage/immortalwrt-all-2.sh
+++ smpackage/immortalwrt-all-2.sh
@@ -107,7 +107,7 @@ NET="package/base-files/files/bin/config_generate"
 ZZZ="package/emortal/default-settings/files/99-default-settings"
 
 #
-sed -i "s#192.168.1.1#192.168.89.249#g" $NET                                                     # 定制默认IP
+sed -i "s#192.168.1.1#192.168.1.249#g" $NET                                                     # 定制默认IP
 # sed -i "s#ImmortalWrt#ImmortalWrt-X86#g" $NET                                          # 修改默认名称为 ImmortalWrt-X86
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ                                          # 取消系统默认密码
 echo "uci set luci.main.mediaurlbase=/luci-static/argon" >> $ZZZ                      # 设置默认主题(如果编译可会自动修改默认主题的，有可能会失效)
